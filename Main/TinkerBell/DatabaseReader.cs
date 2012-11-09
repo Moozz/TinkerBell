4f62f8b21fa1f0b481a5eacf35ba5fc6c1563a24
Nutdanai Phansooksai <fendermoo@gmail.com>
2012-11-09 17:58:30 +0700

    Add RIC and Field to interpreter. You can test it via Mock data in Test() function.

---
+++ Main/TinkerBell/DatabaseReader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+
+namespace TinkerBell
+{
+    class DatabaseReader
+    {
+        static string s_databaseXmlName = "TinkerBell.xml";
+
+        public DatabaseReader()
+        {
+        }
+
+        static public string FindInstrumentDescription(string a_instrument)
+        {
+            return FindAttribute("Instrument", a_instrument, "Description");
+        }
+
+        static public string FindInstrumentType(string a_instrument)
+        {
+            return FindAttribute("Instrument", a_instrument, "Type");
+        }
+
+        static public string FindFieldDescription(string a_field)
+        {
+            return FindAttribute("Field", a_field, "Description");
+        }
+
+        static public string FindFieldType(string a_field)
+        {
+            return FindAttribute("Field", a_field, "Type");
+        }
+
+        static public string FindParameterDescription(string a_parameter)
+        {
+            return FindAttribute("Parameter", a_parameter, "Description1");
+        }
+
+        static private string FindAttribute(string a_element, string a_code, string a_attribute)
+        {
+            XmlDocument l_doc = new XmlDocument();
+            l_doc.Load(s_databaseXmlName);
+            string l_query = "/metadata/" + a_element + "/item[@Code='" + a_code + "']";
+            XmlNode l_xmlNode = l_doc.SelectSingleNode(l_query);
+            if (l_xmlNode == null)
+                return "";
+
+            XmlAttribute l_attribute = l_xmlNode.Attributes[a_attribute];
+            if (l_attribute == null)
+                return "";
+
+            return l_attribute.Value;
+        }
+    }
+}
--- Main/TinkerBell/MainWindow.xaml.cs
+++ Main/TinkerBell/MainWindow.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Xml;
 
 using TinkerBell;
 
@@ -23,28 +22,43 @@ namespace TinkerBell
     public partial class MainWindow : Window, IWillHearMyChilds
     {
         private List<IWillHearMyParent> m_childsWhoAreListeningToMe;
+        private CParameters m_parameter;
 
         public MainWindow()
         {
             InitializeComponent();
             m_childsWhoAreListeningToMe = new List<IWillHearMyParent>();
+            m_parameter = new CParameters();
             test();
         }
 
         public void test()
         {
-            CParameters l_parameter = new CParameters();
-            l_parameter.AddInstrument("EUR=");
-            l_parameter.AddField("ASK");
-            foreach (string l_each in l_parameter.Instruments)
+            m_parameter.AddInstrument("EUR=");
+            m_parameter.AddInstrument("JPY=");
+            m_parameter.AddInstrument("TRI.N");
+            m_parameter.AddInstrument("MSFT.O");
+            m_parameter.AddInstrument("PTT.BK");
+            m_parameter.AddInstrument("AAPL.O");
+            m_parameter.AddInstrument("THB=");
+            m_parameter.AddField("BID");
+            m_parameter.AddField("ASK");
+            m_parameter.AddField("CURRENCY");
+            m_parameter.AddField("CLOSE");
+            m_parameter.AddField("RF.G.Compname");
+            m_parameter.AddField("RF.IS.NetSales");
+            m_parameter.AddField("RI.ID.RIC");
+
+            
+            foreach (string l_each in m_parameter.Instruments)
             {
                 Console.WriteLine(l_each);
             }
-            foreach (string l_each in l_parameter.Fields)
+            foreach (string l_each in m_parameter.Fields)
             {
                 Console.WriteLine(l_each);
             }
-            foreach (KeyValuePair<string, List<string>> l_each in l_parameter.Parameters)
+            foreach (KeyValuePair<string, List<string>> l_each in m_parameter.Parameters)
             {
                 Console.WriteLine(l_each.Key);
                 foreach (string l_eachValue in l_each.Value)
@@ -71,12 +85,58 @@ namespace TinkerBell
             */
 
             // Update Interpreter
+            // This functions returns TYPE1 of FIELD_DES1 and FIELD_DES2 and TYPE2 of FIELD_DES1, FIELD_DES2 
+            // for INS_DES1, INS_DES2 INS_TYPE_OF_1_2 and INS_DES3, INS_DES4 INS_TYPE_OF_3_4
+            if (m_parameter.Instruments.Count != 0 && m_parameter.Fields.Count != 0)
+            {
 
-            string l_interpretedWord = "This functions returns TYPE1 of FIELD_DES1, FIELD_DES2 and TYPE2 of FIELD_DES1, FIELD_DES2 for INS_DES1, INS_DES2 INS_TYPE_OF_1_2 and INS_DES3, INS_DES4 INS_TYPE_OF_3_4";
-               
-            XmlReader l_xml = XmlReader.Create("TinkerBell.xml");
+                string l_interpretedWord = "This function returns ";
+                Dictionary<string, List<string>> l_groupOfField = m_parameter.Fields
+                                                                  .GroupBy(x => DatabaseReader.FindFieldType(x))
+                                                                  .ToDictionary(x => x.Key, x => x.Select(y => DatabaseReader.FindFieldDescription(y)).ToList());
+                foreach (KeyValuePair<string, List<string>> l_elementsOfkey in l_groupOfField)
+                {
+                    l_interpretedWord += l_elementsOfkey.Key + " of ";
+                    for (int i = 0; i < l_elementsOfkey.Value.Count; ++i)
+                    {
+                        l_interpretedWord += l_elementsOfkey.Value[i] + ", ";
+                    }
+                    l_interpretedWord = l_interpretedWord.Remove(l_interpretedWord.Length - ", ".Length);
+                    l_interpretedWord += " and ";
+                }
+                if (l_interpretedWord.LastIndexOf(" and ") > 0)
+                    l_interpretedWord = l_interpretedWord.Remove(l_interpretedWord.Length - " and ".Length);
 
-            InterpreterText.Text = l_interpretedWord;
+                l_interpretedWord += " for ";
+                Dictionary<string, List<string>> l_groupOfInstrument = m_parameter.Instruments
+                                                              .GroupBy(x => DatabaseReader.FindInstrumentType(x))
+                                                              .ToDictionary(x => x.Key, x => x.Select(y => DatabaseReader.FindInstrumentDescription(y)).ToList());
+                foreach (KeyValuePair<string, List<string>> l_elementsOfkey in l_groupOfInstrument)
+                {
+                    for (int i = 0; i < l_elementsOfkey.Value.Count; ++i)
+                    {
+                        l_interpretedWord += l_elementsOfkey.Value[i] + ", ";
+                    }
+                    l_interpretedWord = l_interpretedWord.Remove(l_interpretedWord.Length - ", ".Length);
+                    l_interpretedWord += " " + l_elementsOfkey.Key + " and ";
+                }
+                if (l_interpretedWord.LastIndexOf(" and ") > 0)
+                    l_interpretedWord = l_interpretedWord.Remove(l_interpretedWord.Length - " and ".Length);
+
+                InterpreterText.Text = l_interpretedWord + ".\n";
+                /*
+                if (m_parameter.Parameters.Count != 0)
+                {
+                    foreach (KeyValuePair<string, List<string>> item in m_parameter.Parameters)
+	                {
+		 
+	                } 
+                }*/
+            }
+            else
+            {
+                InterpreterText.Text = "Please add Instrument and Fields :)";
+            }
         }
     }
 }
